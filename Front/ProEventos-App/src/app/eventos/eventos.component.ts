2bd5d2f0029b060ad6942cb8108b0f2532111792
vitor.alves <vitor.alves@reisoffice.com.br>
2022-12-30 13:10:15 -0300

    Table

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
 
   constructor(private http: HttpClient) { }
 
