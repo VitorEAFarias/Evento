8e38f4e155b8ec9ff4ba2e3040acc40d1592672a
VitorEAFarias <vphda7x@gmail.com>
2023-01-02 17:46:24 -0300

    Migrations

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProEventos.API.Data;
-using ProEventos.API.Models;
+using ProEventos.Persistence;
+using ProEventos.Domain;
 
 namespace ProEventos.API.Controllers
 {
@@ -13,8 +13,8 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        private readonly DataContext _context;
-        public EventosController(DataContext context)
+        private readonly ProEventosContext _context;
+        public EventosController(ProEventosContext context)
         {
             _context = context;
         }
@@ -29,7 +29,7 @@ namespace ProEventos.API.Controllers
         public Evento GetById(int id)
         {
             return _context.Eventos.FirstOrDefault(
-                evento => evento.EventoId == id
+                evento => evento.Id == id
             );
         }
 
