4e09d53799e9274f6aa415e76fe147807fc6ac07
VitorEAFarias <vphda7x@gmail.com>
2023-01-03 17:33:42 -0300

    Criando Camadas

--- Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
+++ Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ProEventos.Domain;
 
-namespace ProEventos.Persistence
+namespace ProEventos.Persistence.Contextos
 {
     public class ProEventosContext : DbContext
     {
@@ -20,7 +20,17 @@ namespace ProEventos.Persistence
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<PalestranteEvento>()
-                .HasKey(PE => new {PE.EventoId, PE.PalestranteId});            
+                .HasKey(PE => new {PE.EventoId, PE.PalestranteId});
+
+            modelBuilder.Entity<Evento>()
+                .HasMany(e => e.RedesSociais)
+                .WithOne(rs => rs.Evento)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Palestrante>()
+                .HasMany(e => e.RedesSociais)
+                .WithOne(rs => rs.Palestrante)
+                .OnDelete(DeleteBehavior.Cascade);             
         }
     }
 }
--- Back/src/ProEventos.Persistence/Migrations/20230102203746_Initial.Designer.cs
+++ Back/src/ProEventos.Persistence/Migrations/20230102203746_Initial.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ProEventos.Persistence;
+using ProEventos.Persistence.Contextos;
 
 namespace ProEventos.Persistence.Migrations
 {
