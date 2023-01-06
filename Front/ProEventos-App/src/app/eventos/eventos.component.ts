b963edd74fd0412b3ca437861a6ed61903534b71
VitorEAFarias <vphda7x@gmail.com>
2023-01-06 17:33:59 -0300

    Modal & Spinner

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
 import { Evento } from '../models/Evento';
 import { EventoService } from '../services/evento.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-eventos',
@@ -8,7 +11,7 @@ import { EventoService } from '../services/evento.service';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-
+  modalRef: BsModalRef;
   public eventos: Evento[] = [];
   public eventosFiltrados: Evento[] = [];
 
@@ -34,10 +37,21 @@ export class EventosComponent implements OnInit {
     );
   }
 
-  constructor(private eventoService: EventoService) { }
+  constructor(
+    private modalService: BsModalService,
+    private eventoService: EventoService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) { }
 
   public ngOnInit(): void {
+    this.spinner.show();
     this.getEventos();
+    /** spinner starts on init */
+
+    setTimeout(() => {
+      /** spinner ends afeter 5 seconds */
+    }, 5000);
   }
 
   public changeStatement(): void {
@@ -50,7 +64,24 @@ export class EventosComponent implements OnInit {
         this.eventos = eventos;
         this.eventosFiltrados = this.eventos;
       },
-      error: (error: any) => console.log(error)
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Erro ao carregar os Eventos', 'Erro!');
+      },
+      complete: () => this.spinner.hide()
     });
+  }
+
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef.hide();
+    this.toastr.success('Evento deletado com sucesso.', 'Deletado!')
+  }
+
+  decline(): void {
+    this.modalRef.hide();
   }
 }
